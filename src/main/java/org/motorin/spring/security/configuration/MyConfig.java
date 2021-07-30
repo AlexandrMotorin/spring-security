6957d22c3dd15c9ac525c5676305a7846a9a5c83
AlexandrMotorin <motorinas@gmail.com>
2021-07-31 01:54:59 +0300

    Initial commit

---
+++ src/main/java/org/motorin/spring/security/configuration/MyConfig.java
@@ -0,0 +1,42 @@
+package org.motorin.spring.security.configuration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+@Configuration
+@ComponentScan(value = "org.motorin.spring.security")
+@EnableWebMvc
+public class MyConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        try {
+            comboPooledDataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC");
+            comboPooledDataSource.setUser("bestuser");
+            comboPooledDataSource.setPassword("bestuser");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return comboPooledDataSource;
+    }
+
+}
